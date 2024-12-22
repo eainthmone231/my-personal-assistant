78c8fb48952d093a8717705abfbdb80c5587c698
eainthmone231 <eaintlayhmone.mashalay@gmail.com>
2024-12-22 22:26:17 +0630

    remove env

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@ import streamlit as st
 import openai
 from llama_index.llms.openai import OpenAI
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
